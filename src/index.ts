186af19ebbb006a1eb7aeee978daa03fdd745541
Tony <legendmastertony@gmail.com>
2024-03-12 10:53:27 +0800

    Init

---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import { error, getInput } from '@actions/core'
+import { context, getOctokit } from '@actions/github'
+import assert from 'node:assert'
+
+const token = getInput('token')
+const destRepo = getInput('destination-repository')
+const destToken = getInput('destination-token') || token
+
+const [destRepoOwner, destRepoName] = destRepo.split('/')[0]
+if (!destRepoOwner || !destRepoName) {
+	error('Invalid destination repository')
+}
+
+const srcOctokit = getOctokit(token)
+const destOctokit = getOctokit(destToken)
+
+const srcRelease = await srcOctokit.rest.repos.getLatestRelease({
+	owner: context.repo.owner,
+	repo: context.repo.repo,
+})
+
+const srcAssets = srcRelease.data.assets.map(async (asset) => {
+	const response = await fetch(asset.browser_download_url, {
+		headers: { Authorization: `token ${token}` },
+	})
+	assert(response.ok, `Failed to fetch asset ${asset.name}`)
+	return await response.text()
+})
+const awaitedSrcAssets = await Promise.all(srcAssets)
+
+const destRelease = await destOctokit.rest.repos.createRelease({
+	owner: destRepoOwner,
+	repo: destRepoName,
+	tag_name: srcRelease.data.tag_name,
+	name: srcRelease.data.name || undefined,
+	body: srcRelease.data.body || undefined,
+})
+
+for (const [index, asset] of srcRelease.data.assets.entries()) {
+	destOctokit.rest.repos.uploadReleaseAsset({
+		owner: destRepoOwner,
+		repo: destRepoName,
+		release_id: destRelease.data.id,
+		name: asset.name,
+		data: awaitedSrcAssets[index],
+		headers: { 'content-type': asset.content_type },
+	})
+}
+
+// await Promise.all(
+// 	awaitedSrcAssets.map((asset) =>
+// 		destOctokit.rest.repos.uploadReleaseAsset({
+// 			owner: destRepoOwner,
+// 			repo: destRepoName,
+// 			release_id: destRelease.data.id,
+// 			name: asset.data.name,
+// 			data: ,
+// 		})
+// 	)
+// )
